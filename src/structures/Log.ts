dab95f26f3b937e534503b4622c4c42a0890145f
Leahim <128914772+Leahim2085@users.noreply.github.com>
2023-10-27 23:43:49 +0300

    Update logging system

--- src/structures/Log.ts
+++ src/structures/Log.ts
@@ -1,11 +1,18 @@
-export function Log({ name, description, type }: log): void {
-    const date: Date = new Date();
-    const logTime: string = `\x1b[30m\x1b[11m\x1b[1m${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}\x1b[0m`;
-    return console.log(`${type} ${logTime} ${name} ${description}`);
-}
+export class Logger {
+    private getTime() {
+        const date: Date = new Date();
+        return `\x1b[30m\x1b[11m\x1b[1m${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}\x1b[0m`;
+    } 
 
-interface log {
-    type: string,
-    name: string,
-    description: string
+    public logMain(type: string, info: string) {
+        return console.log(`[\x1b[32m${type.toUpperCase()}\x1b[0m] ${this.getTime()} ${info}`)
+    }
+
+    public logCategory(name: string, info: string ) {
+        return console.log(`[\x1b[36m-\x1b[0m] ${this.getTime()} \x1b[32m${name.toUpperCase()}\x1b[0m ${info}`)
+    }
+
+    public logSubCategory(name: string, info: string) {
+        return console.log(`    [\x1b[36m+\x1b[0m] ${this.getTime()} \x1b[4m${name.toUpperCase()}\x1b[0m ${info}`)
+    }
 }
